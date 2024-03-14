79f40741ac3308e2f8a663b2e1e29822d555153b
Xavier Morales <xmoralesr@uc.cl>
2024-03-14 14:03:40 -0300

    juego del dado_listo

--- juegos/juego_del_dado.py
+++ juegos/juego_del_dado.py
@@ -1,3 +1,5 @@
+import random
+
 def juego_del_dado():
     """
     Esta función tiene que pedirle al usuario que aprete enter para que lance un dado.
@@ -5,4 +7,22 @@ def juego_del_dado():
     Después el computador también tiene que lanzar un dado.
     El primero en sumar 30 puntos gana.
     """
+    suma_jugador = 0
+    suma_computador = 0
+    
+    while suma_jugador < 30 and suma_computador < 30:
+        input('Presione ENTER para lanzar un dado')
+        dado_jugador = random.randint(1,6)
+        suma_jugador += dado_jugador
+        print(f'Has lanzado un dado, y has obtenido un {dado_jugador}. Llevas {suma_jugador} puntos.')
+
+        dado_computador = random.randint(1,6)
+        suma_computador += dado_computador
+        print(f'El computador ha lanzado un dado, y ha obtenido un {dado_computador}. El computador lleva {suma_computador} puntos.')
+
+    if suma_computador >= 30:
+        print(f'Has perdido, computador gano con {suma_computador} puntos.')
+    
+    else:
+        print(f'Has ganado al computador, sumando {suma_jugador} puntos.')
     pass
--- juegos/memoria.py
+++ juegos/memoria.py
@@ -1,8 +1,24 @@
+import random
+
 def memoria():
-    """
-    Esta función representa el juego de memoria.
-    Debes generar una secuencia de números al azar y mostrarla al usuario.
-    Luego, debes pedir al usuario que repita la secuencia.
-    Se debe mostrar un mensaje si el usuario acierta o no.
-    """
+
+    secuencia = []
+    for i in range(5):
+        numero_generado = random.randint(1,20)
+        secuencia.append(str(numero_generado))
+
+    print(f'Memoriza la siguiente secuencia: {secuencia[0]}, {secuencia[1]}, {secuencia[2]}, {secuencia[3]}, {secuencia[4]}')
+
+    respuesta = input("Escribe la secuencia, cuida poner un espacio luego de la coma")
+    respuesta_list = respuesta.split(",")
+    posicion = 0
+    for numero_str in respuesta_list:
+        respuesta_list[posicion] = numero_str.strip(" ")
+    
+    if secuencia == respuesta_list:
+        print('Wow, tienes una memoria sorprendente, bienhecho.')
+    else:
+        print('Secuencia incorrecta, intentalo de nuevo')
+        
+    memoria()
     pass
